99e675a32508d75ba413c9092cbc528b26d7e4aa
Ionescu Alexandru <alx.ionescu97@gmail.com>
2017-11-25 16:38:56 +0200

    added device location

--- app/src/main/java/com/alex/greenroute/presentation/screens/main/MainActivity.java
+++ app/src/main/java/com/alex/greenroute/presentation/screens/main/MainActivity.java
@@ -1,6 +1,12 @@
 package com.alex.greenroute.presentation.screens.main;
 
+import android.Manifest;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import com.alex.greenroute.R;
@@ -11,16 +17,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 import timber.log.Timber;
 
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleMap.OnMarkerClickListener,
         GoogleMap.OnCameraMoveListener,
         GoogleMap.OnCameraMoveCanceledListener,
-        GoogleMap.OnCameraIdleListener {
+        GoogleMap.OnCameraIdleListener,
+        LocationListener {
 
     private GoogleMap mMap;
 
@@ -38,6 +49,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         setupMap();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    /**
+     *      GOOGLE MAPS CALLBACKS
+     */
 
     @Override
     public void onCameraIdle() {
@@ -64,7 +85,38 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Timber.d("onMapReady");
 
         mMap = googleMap;
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(44.439663, 26.096306), 8));
+
+        MainActivityPermissionsDispatcher.setupLocationProviderWithPermissionCheck(this);
+    }
+
+    /**
+     *      LOCATION CALLBACKS
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        Timber.d("Latitude: %f, Longitude: %f", latitude, longitude);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 
     private void setupMap() {
@@ -72,5 +124,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void setupLocationProvider() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+        locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
     }
 }
